a9b75e05bf41c3e4ac0c739548eb20d1a6bb7cbc
sherpa-learnsto-code <learn_sonam@hotmail.com>
2025-07-17 00:54:55 +0545

    changes

--- src/pages/home/DisplaySec.jsx
+++ src/pages/home/DisplaySec.jsx
@@ -1,7 +1,12 @@
 
 export default function DisplaySec() {
   return (
-    <div>
+    <div className="h-[500px] bg-[#F44336] text-white flex justify-center items-center">
+      <div className="text-center space-y-14"> 
+        <h1 className="text-6xl tracking-wider">START PAGE</h1>
+        <p className="font-semibold text-xl">Template by w3.css</p>
+        <button className="bg-black px-5 py-3">Get Started</button>
+      </div>
       
     </div>
   )
