c6e439746ee3b1da758b5f11ad1fd6ba2dae9708
sherpa-learnsto-code <learn_sonam@hotmail.com>
2025-07-19 22:21:20 +0545

    add garne

--- src/pages/home/DisplaySec.jsx
+++ src/pages/home/DisplaySec.jsx
@@ -5,7 +5,7 @@ export default function DisplaySec() {
       <div className="text-center space-y-14"> 
         <h1 className="text-6xl tracking-wider">START PAGE</h1>
         <p className="font-semibold text-xl">Template by w3.css</p>
-        <button className="bg-black px-5 py-3">Get Started</button>
+        <button className="bg-black px-7 py-3">Get Started</button>
       </div>
       
     </div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,16 @@
 import Header from "../../components/Header";
+import Article from "./Article";
 import DisplaySec from "./DisplaySec";
+
 
 export default function HomePage() {
   return (
     <div>
       <Header />
       <DisplaySec />
+      <Article sec={true} />
+      <Article sec={false} />
+
     </div>
   )
 }
